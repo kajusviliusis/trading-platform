bef5babc85dcf86f83694e8fd3fc1488a273d7ad
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-27 14:31:58 +0200

    feat: add stock controller

---
+++ trading-platform/Controllers/StockController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using trading_platform.Data;
+using trading_platform.Models.Entities;
+
+namespace trading_platform.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StockController : ControllerBase
+    {
+        private readonly TradingDbContext _context;
+        public StockController(TradingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetStocks()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStock(int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+        [HttpPost]
+        public IActionResult CreateStock(Stock stock)
+        {
+            _context.Stocks.Add(stock);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetStock), new { id = stock.Id}, stock );
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStock(int id, Stock updated)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            stock.Symbol = updated.Symbol;
+            stock.Name = updated.Name;
+            stock.Price = updated.Price;
+            stock.UpdatedAt = DateTime.UtcNow;
+
+            _context.SaveChanges();
+            return Ok(stock);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStock(int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stock);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
