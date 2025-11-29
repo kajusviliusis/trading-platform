5b3cf8ec7c11ccdcf1df08aacefe0828a6031ed3
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-29 14:55:23 +0200

    feat: add wallet dto

---
+++ trading-platform/Dtos/WalletDto.cs
@@ -0,0 +1,21 @@
+﻿namespace trading_platform.Dtos
+{
+    public class CreateWalletDto
+    {
+        public decimal Balance { get; set; }
+        public string Currency { get; set; }
+        public int UserId { get; set; }
+    }
+    public class UpdateWalletDto
+    {
+        public decimal Balance { get; set; }
+        public string Currency { get; set; } = string.Empty;
+    }
+    public class WalletResponseDto
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public int UserId { get; set; }
+    }
+}
