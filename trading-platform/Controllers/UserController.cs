ca86137921ff0d8035fb1b8d6d2ce1137b34e895
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-29 15:40:52 +0200

    refactor: add dto to usercontroller

--- trading-platform/Controllers/UserController.cs
+++ trading-platform/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using trading_platform.Data;
 using trading_platform.Models.Entities;
+using trading_platform.Dtos;
+using System.Runtime.InteropServices;
 
 namespace trading_platform.Controllers
 {
@@ -19,48 +22,80 @@ namespace trading_platform.Controllers
         [HttpGet]
         public IActionResult GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = _context.Users
+                    .Select(u => new UserResponseDto
+                    {
+                        Id = u.Id,
+                        Username = u.Username,
+                        Email = u.Email,
+                        CreatedAt = u.CreatedAt
+                    })
+                    .ToList();
             return Ok(users);
         }
         [HttpGet("{id}")]
         public IActionResult GetUser(int id)
         {
             var user = _context.Users.Find(id);
-            if (user == null)
+            if (user == null) return NotFound();
+            var response = new UserResponseDto
             {
-                return NotFound();
-            }
-            return Ok(user);
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt
+            };
+            return Ok(response);
         }
         [HttpPost]
-        public IActionResult CreateUser(User user)
+        public IActionResult CreateUser(CreateUserDto dto)
         {
+            var user = new User
+            {
+                Username = dto.Username,
+                Email = dto.Email,
+                PasswordHash = dto.Password //add hashing later
+
+            };
             _context.Users.Add(user);
             _context.SaveChanges();
-            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+
+            var response = new UserResponseDto
+            {
+                Id = user.Id,
+                Username=user.Username,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt
+            };
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, response);
         }
         [HttpPut("{id}")]
-        public IActionResult UpdateUser(int id, User updated)
+        public IActionResult UpdateUser(int id, UpdateUserDto dto)
         {
             var user = _context.Users.Find(id);
-            if(user==null)
-            {
-                return NotFound();
-            }
-            user.Username = updated.Username;
-            user.Email = updated.Email;
-            user.PasswordHash = updated.PasswordHash;
+            if(user==null) return NotFound();
+
+            user.Username = dto.Username;
+            user.Email = dto.Email;
+            
             _context.SaveChanges();
-            return Ok(user);
+
+            var response = new UserResponseDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt
+            };
+
+            return Ok(response);
         }
         [HttpDelete("{id}")]
         public IActionResult DeleteUser(int id)
         {
             var user = _context.Users.Find(id);
-            if(user==null)
-            {
-                return NotFound();
-            }
+            if(user==null) return NotFound();
             _context.Users.Remove(user);
             _context.SaveChanges();
             return NoContent();
--- trading-platform/Dtos/UserDto.cs
+++ trading-platform/Dtos/UserDto.cs
@@ -13,7 +13,7 @@ namespace trading_platform.Dtos
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
     }
-    public class CreateResponseDto
+    public class UserResponseDto
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
