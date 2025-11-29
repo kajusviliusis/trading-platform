82a7fda8117441dde1d77d056b553d7fdabf430d
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-29 14:51:50 +0200

    feat: add user dto

---
+++ trading-platform/Dtos/UserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace trading_platform.Dtos
+{
+    public class CreateUserDto
+        {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        }
+    public class UpdateUserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+    public class CreateResponseDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
