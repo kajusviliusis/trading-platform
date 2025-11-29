08b31bbba8770319557357fc0cddd807947e6bb5
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-29 14:58:19 +0200

    feat: add stock dto

---
+++ trading-platform/Dtos/StockDto.cs
@@ -0,0 +1,22 @@
+﻿namespace trading_platform.Dtos
+{
+    public class CreateStockDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string Name {  get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+    public class UpdateStockDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+    public class StockResponseDto
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
