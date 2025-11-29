94db9b1c11a1da2a805bb063e778bbbd8894139e
kajusviliusis <kajus.viliusis0@gmail.com>
2025-11-29 15:02:03 +0200

    feat: add order dto

---
+++ trading-platform/Dtos/OrderDto.cs
@@ -0,0 +1,20 @@
+﻿namespace trading_platform.Dtos
+{
+    public class CreateOrderDto
+    {
+        public int UserId { get; set; }
+        public int StockId { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; } = string.Empty;
+    }
+    public class OrderResponseDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int StockId { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public decimal PriceAtExecution { get; set; }
+    }
+}
