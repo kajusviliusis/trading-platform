238ca66f9d26e66d9eb1b11864d179adfb4fbc86
kajusviliusis <kajus.viliusis0@gmail.com>
2025-12-01 23:26:09 +0200

    feat: add transaction entity

---
+++ trading-platform/Models/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace trading_platform.Models.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int StockId { get; set; }
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+        public decimal PriceAtExecution { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Type { get; set; }
+    }
+}
