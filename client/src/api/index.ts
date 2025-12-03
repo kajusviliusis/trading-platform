fa3d83a3c5af26d540fb94b898a8350c37eb2c0c
kajusviliusis <kajus.viliusis0@gmail.com>
2025-12-03 23:47:56 +0200

    feat: add api layer to frontend

---
+++ client/src/api/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Order, Holding, Transaction } from "../types";
+
+const API_URL = "http://localhost:7212/api";
+const USER_ID = 1; //hardcoded paskui pakeist 
+
+export async function getOrders(): Promise<Order[]> {
+    const res = await axios.get(`${API_URL}/order`);
+    return res.data;
+}
+
+export async function getHoldings(): Promise<Holding[]> {
+    const res = await axios.get(`${API_URL}/holdings/user/${USER_ID}`);
+    return res.data;
+}
+
+export async function getTransactions(): Promise<Transaction[]> {
+    const res = await axios.get(`${API_URL}/order/user/${USER_ID}/transactions`);
+    return res.data;
+}
+
+export async function createOrder(dto: {
+    stockId: number;
+    quantity: number;
+    type: "BUY" | "SELL";
+}): Promise<Order> {
+    const res = await axios.post(`${API_URL}/order`, { ...dto, userId: USER_ID });
+    return res.data;
+}
