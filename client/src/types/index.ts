a56691512bd42b490fecb211ea3fe4f24e1dd740
kajusviliusis <kajus.viliusis0@gmail.com>
2025-12-03 23:34:53 +0200

    feat: types

---
+++ client/src/types/index.ts
@@ -0,0 +1,27 @@
+export interface Order {
+	id: number;
+	quantity: number;
+	type: "BUY" | "SELL";
+	priceAtExecution: number;
+	timestamp: string;
+	userId: number;
+	stockId: number;
+}
+
+export interface Holding {
+	stockId: number;
+	quantity: number;
+	stockName: string;
+	currentPrice: number;
+}
+
+export interface Transaction {
+	id: number;
+	userId: number;
+	stockId: number;
+	orderId: number;
+	quantity: number;
+	priceAtExecution: number;
+	timestamp: string;
+	type: "BUY" | "SELL";
+}
